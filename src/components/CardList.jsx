92b15f597b863b5922013b368d930df67b7ccfd0
Rolando Oxalc <qj.oxalc@gmail.com>
2022-08-15 13:01:10 -0500

    añadido error boundary

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -2,6 +2,9 @@ import { Card } from "./Card"
 
 
 const CardList = ({ robots }) => {
+    // if (true){
+    //     throw new Error('NOOOOOO!')
+    // }
     return (
         <div>
             {robots.map((robot) => {
